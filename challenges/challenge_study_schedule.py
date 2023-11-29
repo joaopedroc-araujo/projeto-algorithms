c7cb9f3008b424043e707889213cbec7f8e7da27
João <costaaraujojoaopedro@gmail.com>
2023-11-29 20:55:59 -0300

    Implementa função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    if not all(
+        isinstance(i, tuple) and len(i) == 2 for i in permanence_period
+    ):
+        return None
+    return sum(start <= target_time <= end for start, end in permanence_period)
