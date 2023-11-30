3190eceb73ed21dbb16d0147c3b80d04467efdae
João <costaaraujojoaopedro@gmail.com>
2023-11-29 21:10:47 -0300

    FIX: challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,11 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
     if not all(
-        isinstance(i, tuple) and len(i) == 2 for i in permanence_period
+        isinstance(i, tuple)
+        and len(i) == 2
+        and isinstance(i[0], (int, float))
+        and isinstance(i[1], (int, float))
+        for i in permanence_period
     ):
         return None
     return sum(start <= target_time <= end for start, end in permanence_period)
