0ddd9aeba93994df4ddeacfae27cbdf38f240d93
João <costaaraujojoaopedro@gmail.com>
2023-11-29 22:28:08 -0300

    Testes encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("hello", "world")
+    with pytest.raises(TypeError):
+        encrypt_message(42, 2)
+
+    assert encrypt_message("hello", 0) == "olleh"
+    assert encrypt_message("hello", 5) == "olleh"
+    assert encrypt_message("hello", -1) == "olleh"
+
+    assert encrypt_message("hello", 1) == "h_olle"
+    assert encrypt_message("hello", 3) == "leh_ol"
+    assert encrypt_message("hello", 2) == "oll_eh"
+    assert encrypt_message("hello", 4) == "o_lleh"
